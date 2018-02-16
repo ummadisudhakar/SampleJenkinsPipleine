026807e516163209714b5e33a7c58fbe2e88a583
sudhakar_ummadi <sudhakar@italentdigital.com>
2018-02-16 17:11:07 +0530

    removed one moreline

--- src/main/java/jenkinssample/JenkinsSample.java
+++ src/main/java/jenkinssample/JenkinsSample.java
@@ -5,7 +5,7 @@ public class JenkinsSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sample project......");
-		System.out.println("Sample project......");
+		
 		
 	}
 
