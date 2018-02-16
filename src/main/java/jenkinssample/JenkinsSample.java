f556e8b2994698536d5039eb5462fe813421036f
sudhakar_ummadi <sudhakar@italentdigital.com>
2018-02-16 14:19:58 +0530

    New repository added

---
+++ src/main/java/jenkinssample/JenkinsSample.java
@@ -0,0 +1,11 @@
+package jenkinssample;
+
+public class JenkinsSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample project......");
+
+	}
+
+}
