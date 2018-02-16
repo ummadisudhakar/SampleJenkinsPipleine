a7240066e0e32fab86092978058f0a3f1b1db706
sudhakar_ummadi <sudhakar@italentdigital.com>
2018-02-16 17:07:48 +0530

    removed one line

--- src/main/java/jenkinssample/JenkinsSample.java
+++ src/main/java/jenkinssample/JenkinsSample.java
@@ -6,8 +6,7 @@ public class JenkinsSample {
 		// TODO Auto-generated method stub
 		System.out.println("Sample project......");
 		System.out.println("Sample project......");
-		System.out.println("Sample project......");
-
+		
 	}
 
 }
